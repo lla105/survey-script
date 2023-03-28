c6be0cc1ca43a42eddde04edea7b6835f9fe15fb
Leon Lee <leonlee@LeonMBP.local>
2023-03-28 16:52:22 -0700

    checks all boxes that says 10, Excellent, or Take Out, and then submits form

--- nandos.py
+++ nandos.py
@@ -6,58 +6,48 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 import time
 
-# Create a new webdriver instance using chromedriver
-driver = webdriver.Chrome()
-
-
 url = 'http://peri.io/metrotown'
 
 response = requests.get(url)
 html = response.text
 soup = BeautifulSoup(html, 'html.parser')
 
-# Find the radio button element by its value attribute
-radio_button = soup.find('input', {'type': 'radio', 'value': '3695211913'})
 # Find all radio button elements on the page
 radio_buttons = soup.find_all('input', {'type': 'radio'})
-# Extract values of all radio buttons and store them in a list
-radio_button_values = [rb['value'] for rb in radio_buttons]
-print(radio_button_values)
 
-
-payload = {'option': radio_button['value'] }
-radio_button_value = radio_button['value']
-# Open webpage using Selenium
-driver = webdriver.Chrome()
-driver.get(url)
-#Find radio button element by its value and click it
-radio_button = driver.find_element(By.XPATH, f'//input[@type="radio" and @value="{radio_button_value}"]')
-radio_button.click()
-
-# driver.refresh()
-
-response = requests.post(url, data=payload)
-
-if response.status_code == 200:
-    print('Form submitted successfully!')
-else:
-    print('Form submission failed.')
-  
-# Get the value of the radio button
-# radio_button_value = radio_button['value']
-# print(radio_button_value)  # Output: option1
 
 response = requests.get(url)
 html = response.text
 soup = BeautifulSoup(html, 'html.parser')
-target_label = ['No', 'Take Out', 'Excellent', '10']
+# target_label = ['No', 'Take Out', 'Excellent', '10']
+target_label = [ 'Take Out', 'Excellent', '10']
+
+driver = webdriver.Chrome()
+
+value_list = []
 for radio_button in radio_buttons:
     # print(">>>", radio_button.next_sibling.next_sibling.text.strip())
-    if target_label in radio_button.next_sibling.next_sibling.text.strip() :
-        print(target_label)
-    # if target_label in radio_button.next_sibling.strip():
-    #     print("Radio button value:", radio_button['value'])
-    #     break
+    if radio_button.next_sibling.next_sibling.text.strip() in target_label:
+        print(radio_button.next_sibling.next_sibling.text.strip() , " : ", radio_button['value'])
+        driver.get(url)
+        radio_button_value = radio_button['value']
+        value_list.append(radio_button_value)
+        # select_button = driver.find_element(By.XPATH, f'//input[@type="radio" and @value="{radio_button_value}"]')
+        # radio_button.click()
+count = 0
+for radio_button_value in value_list:
+    radio_button = driver.find_element(By.XPATH, f'//input[@type="radio" and @value="{radio_button_value}"]')
+    radio_button.click()
 
-time.sleep(10)
+    # driver.refresh()
+
+submit_button = driver.find_element(By.XPATH, '//button[@type="submit"]')
+submit_button.click()
+
+response = driver.page_source
+if "Form submitted successfully!" in response:
+    print('Form submitted successfuly!')
+else:
+    print('Form submission failed. check for any unchecked boxes')
+time.sleep(100)
 driver.quit()
